c3793348a55f7b96d13c0fbabbc8a3aec0fe35de
typ4rk <typ4rk@gmail.com>
2020-11-04 23:31:00 +0900

    new(back2750):정렬문제

--- a0043/a0043.cpp
+++ a0043/a0043.cpp
@@ -16,7 +16,8 @@ using namespace std;
 #define MAX 8
 std::vector<int> last_card;
 
-#ifdef _USE_FILE
+#ifndef _USE_FILE
+#include <windows.h>
 std::string GetInputFileName()
 {
 	char path[MAX_PATH] = { 0 };
@@ -128,8 +129,100 @@ bool Read()
 	return true;
 }
 
+int T;
+int N;
+int card[8][3];
+int visited[33];
+vector<int> target;
+vector<int> pick;
+vector<int> ans;
+
+void backTracking(int idx, int cnt) {
+	if (cnt == N) {
+		int temp = 0;
+
+		for (int i = 0; i < N; i++) {
+			if (pick[i] < card[i][0]) {
+				temp += card[i][2];
+			}
+			else {
+				for (int j = 2; j >= 0; j--) {
+					if (pick[i] > card[i][j]) {
+						temp += card[i][j];
+						break;
+					}
+				}
+			}
+		}
+
+		ans.push_back(temp);
+		return;
+	}
+	else {
+		//for (int n = idx; n < N; n++) {
+		for (int i = 0; i < N; i++) {
+			if (visited[target[i]] == 1) continue;
+
+			visited[target[i]]++;
+			pick.push_back(target[i]);
+			backTracking(idx + 1, cnt + 1);
+			pick.pop_back();
+			visited[target[i]]--;
+		}
+		//}
+
+	}
+}
+
+void solve03()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	FILE* stream;
+	std::string filename = GetInputFileName();
+	freopen_s(&stream, filename.c_str(), "r", stdin);
+
+	cin >> T;
+
+	for (int t = 1; t <= T; t++) {
+		cin >> N;
+
+		// √ ±‚»≠
+		ans.clear();
+		pick.clear();
+		target.clear();
+		fill_n(visited, 33, 0);
+
+		for (int i = 0; i < 3; i++) {
+			for (int n = 0; n < N; n++) {
+				cin >> card[n][i];
+				visited[card[n][i]]++;
+			}
+		}
+
+		for (int n = 0; n < N; n++) {
+			sort(card[n], card[n] + 3);
+		}
+
+		for (int i = 1; i <= 4 * N; i++) {
+			if (visited[i] == 0) {
+				target.push_back(i);
+			}
+		}
+
+		backTracking(0, 0);
+		sort(ans.begin(), ans.end());
+
+		cout << "#" << t << " " << ans[ans.size() - 1] << "\n";
+	}
+
+	fclose(stream);
+}
 int main()
 {
-	Read();
+	//Read();
+	solve03();
 	return 0;
 }
---
+++ acmipc/2750/2750/2750.cpp
@@ -0,0 +1,52 @@
+﻿// 2750.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "stdio.h"
+#include <iostream>
+#include <algorithm>
+
+int number, data[1000001];
+
+void quickSort(int* data, int start, int end) {
+    if (start >= end) {
+        return;
+    }
+    int key = start;
+    int i = start + 1, j = end, temp;
+    while (i <= j) {
+        while (data[i] <= data[key]) {
+            i++;
+        }
+        while (data[j] >= data[key] && j > start) {
+            j--;
+        }
+        if (i > j) {
+            temp = data[j];
+            data[j] = data[key];
+            data[key] = temp;
+        }
+        else {
+            temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+    }
+    quickSort(data, start, j - 1);
+    quickSort(data, j + 1, end);
+}
+
+int main()
+{
+    int i, j, min, index, temp;
+    scanf("%d", &number);
+    for (i = 0; i < number; i++) {
+        scanf("%d", &data[i]);
+    }
+    //quickSort(data, 0, number - 1);   
+    std::sort(data, data + number);
+
+    for (i = 0; i < number; i++) {
+        printf("%d\n", data[i]);
+    }
+}
+
